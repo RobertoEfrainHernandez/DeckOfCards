c6b0b46d12d6b47b18a087b727ed5aa4d78ddcee
Roberto Efrain Hernandez <bh21346n@pace.edu>
2019-02-27 11:23:53 -0800

    Updated convertCardType method to use a dictionary instead of a switch statement.

--- DeckOfCards/Model/Card.swift
+++ DeckOfCards/Model/Card.swift
@@ -9,49 +9,20 @@
 import Foundation
 
 struct Card: Decodable {
-    let image : String
-    let value : String
-    let suit  : String
-    let code  : String
-    
-    init(image: String, value: String, suit: String, code: String) {
-        self.image = image
-        self.value = value
-        self.suit = suit
-        self.code = code
-    }
-    
-    static func convertCardType(with cardVal: String) -> Int {
-        switch cardVal {
-        case "ACE":
-            return 1
-        case "2":
-            return 2
-        case "3":
-            return 3
-        case "4":
-            return 4
-        case "5":
-            return 5
-        case "6":
-            return 6
-        case "7":
-            return 7
-        case "8":
-            return 8
-        case "9":
-            return 9
-        case "10":
-            return 10
-        case "JACK":
-            return 11
-        case "QUEEN":
-            return 12
-        case "KING":
-            return 13
-        default:
-            break
-        }
-        return -1
-    }
+  let image : String
+  let value : String
+  let suit  : String
+  let code  : String
+  
+  init(image: String, value: String, suit: String, code: String) {
+    self.image = image
+    self.value = value
+    self.suit = suit
+    self.code = code
+  }
+  
+  static func convertCardType(with cardVal: String) -> Int {
+    let cardDict: [String: Int] = ["ACE": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "JACK": 11, "QUEEN": 12, "KING": 13]
+    return cardDict[cardVal] ?? 0
+  }
 }
